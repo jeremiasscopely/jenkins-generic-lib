11765bcbcfb5ac90d816ac75225fadbf29f7e5c3
jeremiasscopely <112541881+jeremiasscopely@users.noreply.github.com>
2022-09-10 19:58:26 +0200

    test

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,3 +1,13 @@
-def call(){
-  echo "Testado!!!!"
+def call(String operationName, Map config = [:]){
+  switch (operationName){
+    case "callMehod1":
+      echo "called method1"
+      break;
+    case "callMehod2":
+      echo "called method2"
+      break;
+    case "callMehod3":
+      echo "called method3"
+      break;
+  }
 }
